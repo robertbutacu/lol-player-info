0d507e87d7f36092182b93052f85556df9f87794
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-05 01:20:59 +0300

    Added PlayerDTO(player summary)

---
+++ WebApplication1/Models/PlayerInformation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [DataContract]
+    public class PlayerInformation
+    {
+        [DataMember]
+        public string name;
+
+        [DataMember]
+        public Dictionary<string, int> lanesPlayed;
+
+        [DataMember]
+        public double averageCsPerMinute;
+
+        [DataMember]
+        public double averageKda;
+
+        [DataMember]
+        public int gamesFed;
+
+        [DataMember]
+        public int gamesCarried;
+
+        [DataMember]
+        public int gamesGotCarried;
+
+        [DataMember]
+        public int averageDmgToChampions;
+
+        [DataMember]
+        public int averageDmgToTurrets;
+
+        [DataMember]
+        public int averageDmgToChampionsEnemies;
+
+        [DataMember]
+        public int averageDmgToTurretsEnemies;
+
+        [DataMember]
+        public int averageDmgToTurretsTeammates;
+
+        [DataMember]
+        public int averageDmgToChampionsTeammates;
+
+        [DataMember]
+        public int averageKills;
+
+        [DataMember]
+        public int averageDeaths;
+
+        [DataMember]
+        public int averageAssists;
+
+    }
+}
