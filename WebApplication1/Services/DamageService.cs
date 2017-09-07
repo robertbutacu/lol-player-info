e51eb6fffda52d013f0699d9c396c9fd347f5be6
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-07 11:06:42 +0300

    Added support for getting any team's dmg.

--- WebApplication1/Services/DamageService.cs
+++ WebApplication1/Services/DamageService.cs
@@ -8,19 +8,81 @@ namespace WebApplication1.Services
 {
     public class DamageService
     {
-        public DamageService ComputeDamageDealtByTeammates(List<MatchDto> matches, long accountId)
+        public DamageDealt GetDamageDealtByTeam(List<MatchDto> matches, long accountId, Boolean isPlayerTeam)
         {
-            return null;
+            DamageDealt dmgDealtByTeam = new DamageDealt();
+
+            matches.ForEach(delegate (MatchDto match)
+            {
+                int playerParticipantId = GetPlayerParticipantId(match.participantsIdentities, accountId);
+
+                int playerTeamId = GetPlayerTeamId(match.participants, playerParticipantId);
+
+                dmgDealtByTeam = ComputeDamageDealtByTeam(match.participants, playerTeamId, playerParticipantId);
+                dmgDealtByTeam = NormalizeDamage(dmgDealtByTeam, matches.Count, isPlayerTeam ? true : false);
+            });
+
+            return dmgDealtByTeam;
         }
 
-        public DamageService ComputeDamageDealtByEnemies(List<MatchDto> matches, long accountId)
-        {
-            return null;
-        }
 
         public DamageService ComputeDamageDealtByPlayer(List<MatchDto> matches, long accountId)
         {
             return null;
         }
+
+
+        private DamageDealt ComputeDamageDealtByTeam(List<ParticipantDto> participants, int teamId, int playerParticipantId)
+        {
+            DamageDealt dmgDealt = new DamageDealt();
+            participants.ForEach(delegate (ParticipantDto participant)
+            {
+                if (participant.teamId == teamId && participant.participantId != playerParticipantId)
+                {
+                    dmgDealt.averageDmgToChampions += participant.stats.totalDamageDealtToChampions;
+                    dmgDealt.averageDmgToTurrets += participant.stats.damageDealtToTurrets;
+                }
+            });
+
+            return dmgDealt;
+        }
+
+
+        private DamageDealt NormalizeDamage(DamageDealt dmgDealt, int totalGames, Boolean isPlayerTeam)
+        {
+            int factor = isPlayerTeam ? 4 : 5;
+
+            dmgDealt.averageDmgToChampions /= (factor * totalGames);
+            dmgDealt.averageDmgToTurrets /= (factor * totalGames);
+
+            return dmgDealt;
+        }
+
+
+        private int GetPlayerTeamId(List<ParticipantDto> participants, int participantId)
+        {
+            int teamId = 0;
+            participants.ForEach(delegate (ParticipantDto participant)
+            {
+                if (participant.participantId == participantId)
+                    teamId = participant.teamId;
+            });
+
+            return teamId;
+        }
+
+
+        private int GetPlayerParticipantId(List<ParticipantIdentityDto> participants, long accountId)
+        {
+            int playerParticipantId = 0;
+            participants.ForEach(delegate (ParticipantIdentityDto participant)
+            {
+                if (participant.player.accountId == accountId)
+                    playerParticipantId = participant.participantId;
+            });
+
+            return playerParticipantId;
+        }
+
     }
 }
