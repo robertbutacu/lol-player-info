cf6ee83739fdf3e64189c7b5e407cbfaff310916
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-08 09:36:50 +0300

    JSON request sent and receives info.

--- WebApplication1/Models/Globals.cs
+++ WebApplication1/Models/Globals.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Models
 {
     public static class Globals
     {
-        public const string API_KEY = "RGAPI-3cf985a8-1ab9-4c70-a021-d541cdbc50c1";
+        public const string API_KEY = "RGAPI-6f1bc980-f33d-4869-b388-c792dbbb072f";
         public static readonly string[] REGIONS = new string[] { "eun1", "na1", "br1", "euw1", "kr", "oc1" };
     }
 }
